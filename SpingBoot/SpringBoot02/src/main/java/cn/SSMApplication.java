5e6f2b897957dc23c4e5adc61d19d601015a4bb9
Zhen77 <Zhen18103690519@gmail.com>
2021-02-06 23:33:48 +0800

    Springboot整合Mybatis和SpringMVC(有bug未解决)

---
+++ SpingBoot/SpringBoot02/src/main/java/cn/SSMApplication.java
@@ -0,0 +1,21 @@
+package cn;
+
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+
+/**
+ * @author : zhen77
+ * @date: 2021/2/6 - 02 - 06 - 23:02
+ * @Description: cn
+ * @version: 1.0
+ */
+@SpringBootApplication{exclude = {DataSourceAutoConfiguration.class}}
+@MapperScan("cn.zhen77.mapper")
+
+public class SSMApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(SSMApplication.class,args);
+    }
+}
--- SpingBoot/SpringBoot02/src/main/java/cn/zhen77/controller/DeptController.java
+++ SpingBoot/SpringBoot02/src/main/java/cn/zhen77/controller/DeptController.java
@@ -1,6 +1,14 @@
 package cn.zhen77.controller;
 
+import cn.zhen77.mapper.DeptMapper;
+import cn.zhen77.pojo.Dept;
+import cn.zhen77.service.DeptService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.List;
 
 /**
  * @author : zhen77
@@ -10,4 +18,20 @@ import org.springframework.stereotype.Controller;
  */
 @Controller
 public class DeptController {
+    @Autowired
+    private DeptService deptService;
+
+    public DeptService getDeptService() {
+        return deptService;
+    }
+
+    public void setDeptService(DeptService deptService) {
+        this.deptService = deptService;
+    }
+
+    @RequestMapping("/getall")
+    @ResponseBody
+    public List<Dept> getall(){
+        return  deptService.selectAll();
+    }
 }
