7f21e33aeb6aa33ddc27b58bf12e949a668a6361
Zhen77 <Zhen18103690519@gmail.com>
2021-02-07 23:28:27 +0800

    SpringBoot整合jsp

---
+++ SpingBoot/SpringBootjspbeta/src/main/java/cn/MyApplication.java
@@ -0,0 +1,17 @@
+package cn;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author : zhen77
+ * @date: 2021/2/7 - 02 - 07 - 23:08
+ * @Description: cn
+ * @version: 1.0
+ */
+@SpringBootApplication
+public class MyApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(MyApplication.class,args);
+    }
+}
