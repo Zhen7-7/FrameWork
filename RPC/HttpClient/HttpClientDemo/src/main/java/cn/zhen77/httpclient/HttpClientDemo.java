257457ae56bd3e09bcb22cdb06ebb3f98a5ebba7
Zhen77 <Zhen18103690519@gmail.com>
2021-02-16 23:04:07 +0800

    实现RPC-get请求+实现RPC-post请求

--- RPC/HttpClient/HttpClientDemo/src/main/java/cn/zhen77/httpclient/HttpClientDemo.java
+++ RPC/HttpClient/HttpClientDemo/src/main/java/cn/zhen77/httpclient/HttpClientDemo.java
@@ -1,17 +1,25 @@
 package cn.zhen77.httpclient;
 
+import org.apache.http.HttpEntity;
+import org.apache.http.NameValuePair;
+import org.apache.http.client.entity.UrlEncodedFormEntity;
 import org.apache.http.client.methods.CloseableHttpResponse;
 import org.apache.http.client.methods.HttpGet;
+import org.apache.http.client.methods.HttpPost;
 import org.apache.http.client.methods.HttpUriRequest;
 import org.apache.http.client.utils.URIBuilder;
 import org.apache.http.impl.client.CloseableHttpClient;
 import org.apache.http.impl.client.HttpClients;
+import org.apache.http.message.BasicNameValuePair;
 import org.apache.http.util.EntityUtils;
 import org.junit.Test;
 
 import javax.swing.text.html.parser.Entity;
 import java.io.Closeable;
+import java.io.IOException;
 import java.net.URISyntaxException;
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * @author : zhen77
@@ -44,6 +52,36 @@ public class HttpClientDemo {
         } catch (Exception e) {
             e.printStackTrace();
         }
+
+    }
+    @Test
+    public void testPostDemo(){
+        try {
+            //1.创建http工具（理解成:浏览器） 发起请求，解析响应
+            CloseableHttpClient httpClient = HttpClients.createDefault();
+            //2.创建HttpPOST请求对象
+            HttpPost post = new HttpPost("http://localhost:8080/demo");
+            //2.1 创建请求参数
+            List<NameValuePair> params = new ArrayList<>();
+            //2.2因为NameValuePair是接口不能直接获取,因为要先获取其实现类BasicNamevaluePair
+            params.add(new BasicNameValuePair("param","123"));
+            //2.3创建HttpEntity接口的文本实现类的对象，放入参数并设置编码
+            HttpEntity httpEntity = new UrlEncodedFormEntity(params,"utf-8");//多态,接口类型指向实现类
+            //2.4放入到HttpPost对象中
+            post.setEntity(httpEntity);
+            //3.创建响应对象
+            CloseableHttpResponse response = httpClient.execute(post);
+            //4.由于响应体是字符串，因此把HttpEntity类型转换为字符串类型
+            String result = EntityUtils.toString(response.getEntity());
+            //5.输出结果
+            System.out.println(result);
+            //6.释放资源
+            response.close();
+            httpClient.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
     }
 }
 //结果:zhangsanabc
